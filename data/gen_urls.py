cf5ad8798e514cf59171e8b7270bc1ebcd9e151f
Marcin Kaszynski <marcin.kaszynski@gmail.com>
2013-04-21 15:32:57 +0200

    Commit before refactor for 2 crafts.

--- data/gen_urls.py
+++ data/gen_urls.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python
 import json
 
-data = eval(open('vger1.out').read())
+def convert(f_in, f_out):
+    data = eval(open(f_in).read())
+    open(f_out, 'w').write(json.dumps(dict([(y, url) for (y, h, tgt, url) in data]).items()))
 
-print json.dumps(dict([(y, url) for (y, h, tgt, url) in data]).items())
+convert('vger1.out', '../html/urls_vger1.json')
+convert('vger2.out', '../html/urls_vger2.json')
+
